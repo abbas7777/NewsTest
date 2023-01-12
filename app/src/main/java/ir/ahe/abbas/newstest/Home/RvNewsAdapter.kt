fa24c88e9520f0d1d10931f0aca8c0297b6ab4e3
Abbass <melika.zabihi.n@gmail.com>
2023-01-12 09:27:38 +0330

    Api

--- app/src/main/java/ir/ahe/abbas/newstest/Home/RvNewsAdapter.kt
+++ app/src/main/java/ir/ahe/abbas/newstest/Home/RvNewsAdapter.kt
@@ -17,6 +17,8 @@ class RvNewsAdapter (var c:Context,var newsList: List<News>) : RecyclerView.Adap
 
 
 
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RvNewsViewHolder {
 
         return RvNewsViewHolder(LayoutInflater.from(c).inflate(R.layout.item_news,parent,false))
