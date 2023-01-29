9128ec1ffed9140fa30779bc5edd29ec4ea8739c
Abbas <abbas.ha.1378@gmail.com>
2023-01-29 14:24:06 +0330

    delete xml views of MainActivity.kt

--- app/src/main/java/ir/ahe/abbas/newstest/main/MainActivity.kt
+++ app/src/main/java/ir/ahe/abbas/newstest/main/MainActivity.kt
@@ -1,33 +1,18 @@
 package ir.ahe.abbas.newstest.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
+import androidx.activity.ComponentActivity
 import dagger.hilt.android.AndroidEntryPoint
-import ir.ahe.abbas.newstest.R
-import ir.ahe.abbas.newstest.databinding.ActivityMainBinding
+import androidx.activity.compose.setContent
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root as View)
-
-        setUpViews()
-    }
-
-    private fun setUpViews() {
-        val bnvMain=binding.btmMainActivityMenu
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navC = navHostFragment.navController
-
-        bnvMain.setupWithNavController(navC)
+        setContent{}
 
     }
+
 }
