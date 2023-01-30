fc740a7cb0780da04fb58b0df045246ffb28105a
Abbas <abbas.ha.1378@gmail.com>
2023-01-30 10:37:37 +0330

    setup bottomNavigation

--- app/src/main/java/ir/ahe/abbas/newstest/main/MainActivity.kt
+++ app/src/main/java/ir/ahe/abbas/newstest/main/MainActivity.kt
@@ -4,6 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import dagger.hilt.android.AndroidEntryPoint
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -11,8 +24,80 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent{}
 
+        setContent {
+            AppScreen()
+        }
+
+    }
+
+    @Composable
+    private fun AppScreen() {
+
+        val itemList= mutableListOf(
+            ScreenItem.Home,
+            ScreenItem.Category
+        )
+
+        BottomNavigation(itemList = itemList)
+
+    }
+
+    @Composable
+    private fun BottomNavigation(
+        itemList: MutableList<ScreenItem>
+    ) {
+
+        val navController = rememberNavController()
+
+        Scaffold(
+            bottomBar = {
+                BottomNavigation {
+
+                    val navBackStackEntry by navController.currentBackStackEntryAsState()
+                    val currentDestination = navBackStackEntry?.destination
+
+                    itemList.forEach { item ->
+
+                        BottomNavigationItem(
+                            icon = {
+                                Icon(
+                                    painter = painterResource(id = item.drawableRes),
+                                    contentDescription = null
+                                )
+                            },
+                            label = { Text(text = stringResource(id = item.resourceId)) },
+                            selected = currentDestination?.hierarchy?.any
+                            { it.route == item.route } == true,
+                            onClick = {
+                                navController.navigate(item.route) {
+
+                                    popUpTo(navController.graph.findStartDestination().id) {
+                                        saveState = true
+                                    }
+                                    launchSingleTop = true
+                                    restoreState = true
+                                }
+                            }
+                        )
+
+                    }
+                }
+            }
+        ) { innerPadding ->
+            NavHost(
+                navController, startDestination = ScreenItem.Home.route,
+                Modifier.padding(innerPadding)
+            ) {
+               // composable()
+            }
+        }
+    }
+
+    @Preview
+    @Composable
+    private fun Preview() {
+        AppScreen()
     }
 
 }
