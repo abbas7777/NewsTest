6763fa11cb79f907a65b36bd25c34da28337d6c4
Abbas <abbas.ha.1378@gmail.com>
2023-02-01 09:42:09 +0330

    change news to MutableStateFlow

--- app/src/main/java/ir/ahe/abbas/newstest/home/HomeViewModel.kt
+++ app/src/main/java/ir/ahe/abbas/newstest/home/HomeViewModel.kt
@@ -4,18 +4,30 @@ import androidx.lifecycle.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import ir.ahe.abbas.newstest.models.News
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.MutableStateFlow
+
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor( private val homeRepository: HomeRepository) : ViewModel() {
 
-    private var _news: MutableLiveData<List<News>> =MutableLiveData()
-    var news: LiveData<List<News>> = _news
+
+    init {
+        getNews(
+            "Apple",
+            "2023-01-12",
+            "popularity",
+            "79819d81c81c4b5aa23c25e99ce15029"
+        )
+    }
+
+
+    var news = MutableStateFlow<List<News>>(emptyList())
 
     fun getNews(q:String, from:String, sortBy :String, apiKey: String){
         viewModelScope.launch{
             homeRepository.getNews(q, from, sortBy, apiKey).collect {
-                _news.postValue(it)
+                news.emit(it)
             }
 
         }
