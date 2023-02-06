f04b709a02d7779f5c35e675251056f820afeba3
Abbas <abbas.ha.1378@gmail.com>
2023-02-06 14:30:09 +0330

    create HomeDbDataSource and get list of news from Room

---
+++ app/src/main/java/ir/ahe/abbas/newstest/home/HomeDbDataSource.kt
@@ -0,0 +1,13 @@
+package ir.ahe.abbas.newstest.home
+
+import ir.ahe.abbas.newstest.database.NewsDao
+import ir.ahe.abbas.newstest.database.NewsEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class HomeDbDataSource @Inject constructor(private val newsDao: NewsDao) {
+
+    fun getNewsFromLocal(): Flow<List<NewsEntity>> {
+        return newsDao.getAllNews()
+    }
+}
