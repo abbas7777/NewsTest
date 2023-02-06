2b4d47bbd05113a7f29c29106016135d4ee9d4fa
Abbas <abbas.ha.1378@gmail.com>
2023-02-06 15:12:19 +0330

    get list of newsEntity from HomeDbDataSource and map it to News

--- app/src/main/java/ir/ahe/abbas/newstest/home/HomeRepository.kt
+++ app/src/main/java/ir/ahe/abbas/newstest/home/HomeRepository.kt
@@ -3,14 +3,35 @@ package ir.ahe.abbas.newstest.home
 import ir.ahe.abbas.newstest.models.News
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
-class HomeRepository @Inject constructor( private var homeApiDataSource: HomeApiDataSource) {
+class HomeRepository @Inject constructor(
+    private var homeApiDataSource: HomeApiDataSource,
+    private var homeDbDataSource: HomeDbDataSource,
+) {
 
-    suspend fun getNews(q:String, from:String, sortBy :String, apiKey: String):Flow<List<News>>{
+    suspend fun getNews(q: String, from: String, sortBy: String, apiKey: String): Flow<List<News>> {
 
         return flow {
             homeApiDataSource.getNews(q, from, sortBy, apiKey).articles?.let { emit(it) }
+        }
+    }
+
+    fun getNewsLocal(): Flow<List<News>> {
+        return homeDbDataSource.getNewsFromLocal().map {
+            it.map { news ->
+                News(
+                    news.source,
+                    news.author,
+                    news.title,
+                    news.description,
+                    news.url,
+                    news.urlToImage,
+                    news.publishedAt,
+                    news.content
+                )
+            }
         }
     }
 
