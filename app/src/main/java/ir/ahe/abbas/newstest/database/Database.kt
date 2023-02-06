68d20d8213007dc3bffc3cb6e0a5c952a37613fb
Abbas <abbas.ha.1378@gmail.com>
2023-02-06 11:38:26 +0330

    create NewsEntity and define columns

---
+++ app/src/main/java/ir/ahe/abbas/newstest/database/Database.kt
@@ -0,0 +1,9 @@
+package ir.ahe.abbas.newstest.database
+
+import androidx.room.RoomDatabase
+import androidx.room.Database
+
+@Database(entities = [], version = 1, exportSchema = false)
+abstract class Database : RoomDatabase() {
+
+}
