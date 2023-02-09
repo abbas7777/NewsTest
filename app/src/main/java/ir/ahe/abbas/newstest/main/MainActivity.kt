6ba82217513e4fae0eaffa4b78f5f861ad9f70f4
Abbas <abbas.ha.1378@gmail.com>
2023-02-09 12:42:35 +0330

    update:<MainActivity> move Scaffold to AppScreen method

--- app/src/main/java/ir/ahe/abbas/newstest/main/MainActivity.kt
+++ app/src/main/java/ir/ahe/abbas/newstest/main/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
@@ -28,68 +29,62 @@ class MainActivity : ComponentActivity() {
         setContent {
             AppScreen()
         }
-
     }
 
     @Composable
     private fun AppScreen() {
 
-        val itemList= mutableListOf(
+        val itemList = listOf(
             BottomNavItem.Home,
             BottomNavItem.Category
         )
-
-        BottomNavigation(itemList = itemList)
-
-    }
-
-    @Composable
-    private fun BottomNavigation(
-        itemList: MutableList<BottomNavItem>
-    ) {
-
         val navController = rememberNavController()
 
         Scaffold(
-            bottomBar = {
-                BottomNavigation {
-
-                    val navBackStackEntry by navController.currentBackStackEntryAsState()
-                    val currentDestination = navBackStackEntry?.destination
-
-                    itemList.forEach { item ->
-
-                        BottomNavigationItem(
-                            icon = {
-                                Icon(
-                                    painter = painterResource(id = item.drawableRes),
-                                    contentDescription = null
-                                )
-                            },
-                            label = { Text(text = stringResource(id = item.label)) },
-                            selected = currentDestination?.hierarchy?.any
-                            { it.route == item.route } == true,
-                            onClick = {
-                                navController.navigate(item.route) {
-
-                                    popUpTo(navController.graph.findStartDestination().id) {
-                                        saveState = true
-                                    }
-                                    launchSingleTop = true
-                                    restoreState = true
-                                }
-                            }
-                        )
-
-                    }
-                }
-            }
+            bottomBar = { BottomNavigation(itemList = itemList, navController) }
         ) { innerPadding ->
             NavHost(
                 navController, startDestination = BottomNavItem.Home.route,
                 Modifier.padding(innerPadding)
             ) {
-               // composable()
+            }
+        }
+    }
+
+    @Composable
+    private fun BottomNavigation(
+        itemList: List<BottomNavItem>,
+        navController: NavController
+    ) {
+
+        BottomNavigation {
+
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentDestination = navBackStackEntry?.destination
+
+            itemList.forEach { item ->
+
+                BottomNavigationItem(
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = item.drawableRes),
+                            contentDescription = null
+                        )
+                    },
+                    label = { Text(text = stringResource(id = item.label)) },
+                    selected = currentDestination?.hierarchy?.any
+                    { it.route == item.route } == true,
+                    onClick = {
+                        navController.navigate(item.route) {
+
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    }
+                )
             }
         }
     }
